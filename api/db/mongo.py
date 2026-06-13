1790a96059d057e528fa9f6986a049f17cbbe8c5
LioraYacob-Uni <liorayacob@mail.tau.ac.il>
2026-06-13 21:19:42 +0300

    feat: add restaurant filter, top popular restaurants, and Mongo tests

--- api/db/mongo.py
+++ api/db/mongo.py
@@ -1,13 +1,18 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
-load_dotenv()
+BASE_DIR = Path(__file__).resolve().parents[1]  # תיקיית api
+load_dotenv(BASE_DIR / ".env")
 
 MONGO_URI = os.getenv("MONGO_URI")
 
 client = MongoClient(MONGO_URI)
 
-db = client["vibedine"]
+db = client["restaurants_project_db"]
 
 users_collection = db["users"]
+restaurants_collection = db["restaurants"]
+
+print(client.list_database_names())
