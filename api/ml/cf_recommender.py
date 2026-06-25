19729c407f6c31edfa8309bf612b398a0a014118
LioraYacob-Uni <liorayacob@mail.tau.ac.il>
2026-06-25 12:46:02 +0300

    feat: add popular restaurants recommendation in ml

--- api/ml/cf_recommender.py
+++ api/ml/cf_recommender.py
@@ -112,90 +112,6 @@ def recommend_for_user_cf_augmented(user_id, top_k=config.TOP_K, candidate_gmap_
     return recommendations
 
 
-### get Top-K restaurant recommendations for a user using Item-Based Collaborative Filtering
-# def recommend_for_user_cf(user_id, top_k=config.TOP_K, candidate_gmap_ids=None, allow_liked_items=False):
-#     # allow_liked_items: allow restaurants already liked by the user to appear in recommendations.
-    
-#     if user_id not in user_id_to_index:
-#         print(f"User {user_id} not found")
-#         return []
-
-#     user_idx = user_id_to_index[user_id]
-
-#     user_row = user_item_matrix[user_idx]
-
-#     liked_indices = user_row.nonzero()[1]
-
-#     if len(liked_indices) == 0:
-#         print(f"No liked restaurants found for user {user_id}")
-#         return []
-
-#     liked_ratings = user_row.data
-
-#     # Weighted Item-Based CF prediction:
-#     # score = sum(similarity × rating) / sum(similarity)
-#     #
-#     # Higher user ratings have stronger influence on the recommendation score.
-#     # Item similarities are based on behavior of all users in item_user_matrix.
-
-#     similarities = cosine_similarity(item_user_matrix[liked_indices], item_user_matrix)
-
-#     # Remove self-similarity:
-#     # do not let a restaurant contribute to its own score with similarity = 1    
-#     for i, item_idx in enumerate(liked_indices):
-#         similarities[i, item_idx] = 0
-
-
-#     numerator = similarities.T @ liked_ratings
-#     denominator = similarities.sum(axis=0)
-
-#     scores = np.divide(
-#         numerator, denominator, out=np.zeros_like(numerator), where=denominator != 0
-#     )
-
-#     # Sort first by predicted rating.
-#     # If multiple restaurants have the same predicted rating,
-#     # prefer the ones with stronger similarity support.
-#     ranked_indices = sorted(
-#         range(len(scores)),
-#         key=lambda idx: (round(float(scores[idx]), 3), denominator[idx]),
-#         reverse=True,
-#     )
-
-#     liked_set = set(liked_indices)
-#     recommendations = []
-#     candidate_set = set(candidate_gmap_ids) if candidate_gmap_ids is not None else None
-
-#     for idx in ranked_indices:
-#         if not allow_liked_items and idx in liked_set:
-#             continue
-
-#         gmap_id = index_to_item_id[idx]
-
-#         if candidate_set is not None and gmap_id not in candidate_set:
-#             continue
-
-#         name = (
-#             restaurant_lookup.loc[gmap_id]["name"]
-#             if gmap_id in restaurant_lookup.index
-#             else "Unknown"
-#         )
-
-#         recommendations.append(
-#             {
-#                 "gmap_id": gmap_id,
-#                 "name": name,
-#                 "predicted_rating": round(float(scores[idx]), 3),
-#                 "similarity_strength": round(float(denominator[idx]), 3),
-#             }
-#         )
-
-#         if len(recommendations) == top_k:
-#             break
-
-#     return recommendations
-
-
 def recommend_for_group_cf(user_ids, top_k=10, per_user_k=50, candidate_gmap_ids=None, online_likes_by_user=None):
     """
     Generate recommendations for a group of users
@@ -286,6 +202,131 @@ def get_offline_likes(user_id, min_rating=config.MIN_RATING):
     user_row = user_item_matrix[user_idx]
 
     return int((user_row.data >= min_rating).sum())
+
+
+def get_popular_restaurants(top_k=config.TOP_K):
+    """
+    Return the Top-K most popular restaurants
+    based on offline positive ratings.
+    """
+
+    # Sum positive ratings (4-5) for each restaurant
+    popularity_scores = np.asarray(
+        user_item_matrix.sum(axis=0)
+    ).ravel()
+
+    # Rank restaurants by popularity
+    ranked_indices = popularity_scores.argsort()[::-1]
+
+    recommendations = []
+
+    # Build recommendation list
+    for idx in ranked_indices[:top_k]:
+        gmap_id = index_to_item_id[idx]
+
+        name = (
+            restaurant_lookup.loc[gmap_id]["name"]
+            if gmap_id in restaurant_lookup.index
+            else "Unknown"
+        )
+
+        recommendations.append(
+            {
+                "gmap_id": gmap_id,
+                "name": name,
+                "popularity_score": round(float(popularity_scores[idx]), 3),
+            }
+        )
+
+    return recommendations
+
+
+
+
+
+### get Top-K restaurant recommendations for a user using Item-Based Collaborative Filtering
+# def recommend_for_user_cf(user_id, top_k=config.TOP_K, candidate_gmap_ids=None, allow_liked_items=False):
+#     # allow_liked_items: allow restaurants already liked by the user to appear in recommendations.
+    
+#     if user_id not in user_id_to_index:
+#         print(f"User {user_id} not found")
+#         return []
+
+#     user_idx = user_id_to_index[user_id]
+
+#     user_row = user_item_matrix[user_idx]
+
+#     liked_indices = user_row.nonzero()[1]
+
+#     if len(liked_indices) == 0:
+#         print(f"No liked restaurants found for user {user_id}")
+#         return []
+
+#     liked_ratings = user_row.data
+
+#     # Weighted Item-Based CF prediction:
+#     # score = sum(similarity × rating) / sum(similarity)
+#     #
+#     # Higher user ratings have stronger influence on the recommendation score.
+#     # Item similarities are based on behavior of all users in item_user_matrix.
+
+#     similarities = cosine_similarity(item_user_matrix[liked_indices], item_user_matrix)
+
+#     # Remove self-similarity:
+#     # do not let a restaurant contribute to its own score with similarity = 1    
+#     for i, item_idx in enumerate(liked_indices):
+#         similarities[i, item_idx] = 0
+
+
+#     numerator = similarities.T @ liked_ratings
+#     denominator = similarities.sum(axis=0)
+
+#     scores = np.divide(
+#         numerator, denominator, out=np.zeros_like(numerator), where=denominator != 0
+#     )
+
+#     # Sort first by predicted rating.
+#     # If multiple restaurants have the same predicted rating,
+#     # prefer the ones with stronger similarity support.
+#     ranked_indices = sorted(
+#         range(len(scores)),
+#         key=lambda idx: (round(float(scores[idx]), 3), denominator[idx]),
+#         reverse=True,
+#     )
+
+#     liked_set = set(liked_indices)
+#     recommendations = []
+#     candidate_set = set(candidate_gmap_ids) if candidate_gmap_ids is not None else None
+
+#     for idx in ranked_indices:
+#         if not allow_liked_items and idx in liked_set:
+#             continue
+
+#         gmap_id = index_to_item_id[idx]
+
+#         if candidate_set is not None and gmap_id not in candidate_set:
+#             continue
+
+#         name = (
+#             restaurant_lookup.loc[gmap_id]["name"]
+#             if gmap_id in restaurant_lookup.index
+#             else "Unknown"
+#         )
+
+#         recommendations.append(
+#             {
+#                 "gmap_id": gmap_id,
+#                 "name": name,
+#                 "predicted_rating": round(float(scores[idx]), 3),
+#                 "similarity_strength": round(float(denominator[idx]), 3),
+#             }
+#         )
+
+#         if len(recommendations) == top_k:
+#             break
+
+#     return recommendations
+
 
 
 
